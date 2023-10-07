f7c887cbe7660e327c4823b591e5655590115b22
bautistallobeta <bautillobeta@gmail.com>
2023-10-07 10:30:32 -0300

    archivo test inicializado

--- test/lista.js
+++ test/lista.js
@@ -4,19 +4,23 @@
  * https://www.microprocesadores.unt.edu.ar/
  *
  * Copyright (c) 2023, Bautista José Llobeta <llobetabautista@gmail.com>
+
  *
  * SPDX-License-Identifier: MIT
  */
-
 
 const describe = require('mocha').describe
 const it = require('mocha').it
 const assert = require('chai').assert
 
+const lista = require('../src/lista').lista
+
 describe('En una lista de pares clave:valor ...', function () {
-    describe('cuando la lista esta vacia ...', function () {
-        it('hay cero elementos', function () {
-            assert.fail('empezamos')
-        })
+  describe('cuando la lista esta vacia ...', function () {
+    //Cuando la lista esta vacia
+    it('hay cero elementos', function () {
+      const nueva_lista = lista.crear()
+      assert.equal(nueva_lista.length, 0)
     })
+  })
 })
