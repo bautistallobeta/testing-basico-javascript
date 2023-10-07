1963639a405ee4c106d04bb15352e6bade628b3a
bautistallobeta <bautillobeta@gmail.com>
2023-10-07 09:53:48 -0300

    configs y licencias

--- resources/license.js
+++ resources/license.js
@@ -3,7 +3,8 @@
  * Facultad de Ciencias Exactas y Tecnología, Universidad Nacional de Tucumán
  * https://www.microprocesadores.unt.edu.ar/
  *
- * Copyright (c) 2023, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+ * Copyright (c) 2023, Bautista José Llobeta <llobetabautista@gmail.com>
+
  *
  * SPDX-License-Identifier: MIT
  */
---
+++ test/lista.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2023, Laboratorio de Microprocesadores
+ * Facultad de Ciencias Exactas y Tecnología, Universidad Nacional de Tucumán
+ * https://www.microprocesadores.unt.edu.ar/
+ *
+ * Copyright (c) 2023, Bautista José Llobeta <llobetabautista@gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+
+const describe = require('mocha').describe
+const it = require('mocha').it
+const assert = require('chai').assert
+
+describe('En una lista de pares clave:valor ...', function () {
+    describe('cuando la lista esta vacia ...', function () {
+        it('hay cero elementos', function () {
+            assert.fail('empezamos')
+        })
+    })
+})
